3c7fefbdde4514eeed2da433be54e029f976fbe8
Adin Ackerman <adinackerman@gmail.com>
2023-09-21 17:47:09 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-app = Flask(__name__)
+app = Flask(__name__, static_url_path = '/static')
 
 
 @app.route('/')

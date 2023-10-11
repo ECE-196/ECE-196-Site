1b7fd0d245be2806311798ab428d4e9fa538ef2e
Canyon <canyoncavicke@gmail.com>
2023-10-10 23:19:23 -0700

    Further Development
    Layouts implemented for multiple pages.

--- app.py
+++ app.py
@@ -8,5 +8,13 @@ def index():
     return send_file('index.html')
 
 
+@app.route('/technicalworkshops')
+def techworkshops():
+    return send_file('technicalworkshops.html')
+
+@app.route('/professionalworkshops')
+def proworkshops():
+    return send_file('professionalworkshops.html')
+
 if __name__ == '__main__':
     app.run()
